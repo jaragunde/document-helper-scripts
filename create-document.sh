0219bf48e8c0a8b5b6ed775affa411d1eeb7e534
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2013-11-11 20:38:45 +0100

    create-document: first version.
    Compresses a set of files into a file called document.docx.

---
+++ create-document.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# create-document.sh - Creates an OOXML document file from a set of
+# uncompressed sources.
+# Copyright (C) 2013 Igalia, S.L. <info@igalia.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# TODO:
+# * Support file paths with spaces
+# * Parameter to specify the output document name.
+# * Check that any spacing is removed from the XML sources.
+
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+DOCUMENT_FILES_PATH=$1
+CURRENT_PATH=`pwd`
+TMP_PATH="/tmp"
+
+# check document exists
+[ -n "$DOCUMENT_FILES_PATH" ] || die "No path provided"
+[ -e $DOCUMENT_FILES_PATH ]   || die "Directory $DOCUMENT_FILES_PATH does not exist"
+
+# remove old document
+rm document.docx
+
+# compress files
+cd $DOCUMENT_FILES_PATH
+zip -r $TMP_PATH/document *
+
+# rename to docx
+mv $TMP_PATH/document.zip $CURRENT_PATH/document.docx
+
